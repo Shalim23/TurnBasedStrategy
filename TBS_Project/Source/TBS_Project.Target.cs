5e923ca0e102ce832290e796c3477ed694845f6f
Dmytro Shalimov <Dmytro.Shalimov@ubisoft.com>
2020-01-10 11:49:29 +0200

    client init commit

---
+++ TBS_Project/Source/TBS_Project.Target.cs
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class TBS_ProjectTarget : TargetRules
+{
+	public TBS_ProjectTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+
+		ExtraModuleNames.AddRange( new string[] { "TBS_Project" } );
+	}
+}
---
+++ TBS_Project/Source/TBS_Project/TBS_ProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TBS_ProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TBS_PROJECT_API ATBS_ProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
